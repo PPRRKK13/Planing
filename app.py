ca586b8a0fdaddcea1466541489417deb0c94e4a
PPRRKK13 <kreklis@gmail.com>
2025-04-17 15:56:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ file_path = os.path.join(current_dir, "For Phyton.xlsx")
 @st.cache_data
 def load_data():
     if not os.path.exists(file_path):
-        st.error("❌ Excel file 'For Phyton.xlsx' not found. Please ensure it's in the same directory as 'app.py'.")
-        st.stop()
+        
     xls = pd.ExcelFile(file_path)
     table_df = xls.parse("Table")
     item_df = xls.parse("Item sizes per meter")
