2b3a8a6f62651b8c96d9f667e1f6c989f3a47bd6
PPRRKK13 <kreklis@gmail.com>
2025-04-23 14:16:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from datetime import timedelta
 def load_data():
     xls = pd.ExcelFile("For Phyton.xlsx")
     table_df = xls.parse("Table")
-    item_df = xls.parse("Item sizes per meter")
-    speed_df = xls.parse("Speed")
-    hours_df = xls.parse("Hours")
+    item_df = xls.parse("Item Sizes per meter")
+    speed_df = xls.parse("Manufacturing speed")
+    hours_df = xls.parse("Hours per day")
     holiday_df = xls.parse("Holidays")  # Sheet with one column 'Date' in datetime format
     return table_df, item_df, speed_df, hours_df, holiday_df
 
